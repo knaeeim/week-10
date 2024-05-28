7d0f80e8f59910c131a3a8451bd705d8c23fcccf
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2024-05-28 21:53:27 +0530

    added day-3

---
+++ Day-3/B_Two_Binary_Strings.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using order_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define vi vector<int>
+#define vll vector<ll>
+#define vp vector<pair<ll,ll>>
+#define sorta(arr) sort(arr.begin(), arr.end());
+#define sortd(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique((X).begin(),(X).end()),(X).end())
+#define range(arr) for(auto el: arr) cout<<el<<" ";
+
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    
+
+    int t; cin>>t; 
+
+    while(t--){
+        string s; cin>>s; 
+        string p; cin>>p; 
+        bool flag = false;
+        for(int i = 0; i < s.size(); i++){
+            if((s[i] == '0' and p[i] == '0') and (s[i+1] == '1' and p[i+1] == '1')){
+                flag = true;
+                break;
+            }
+        }
+
+        if(flag) yes;
+        else no; 
+    }
+    return 0; 
+}
