42da0fe63d146b3632eee51ab6fb119b00e05735
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2024-06-01 22:01:40 +0530

    added day-7

---
+++ Day-7/H_1_Maximum_Crossings_Easy_Version.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using order_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define vi vector<int>
+#define vll vector<ll>
+#define vp vector<pair<ll,ll>>
+#define sorta(arr) sort(arr.begin(), arr.end());
+#define sortd(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique((X).begin(),(X).end()),(X).end())
+#define range(arr) for(auto el: arr) cout<<el<<" ";
+
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    
+
+    int t; cin>>t; 
+
+    while(t--){
+        ll n; cin>>n; 
+
+        vll v(n); 
+        for(int i = 0; i < n; i++) cin>>v[i]; 
+
+        order_set <ll> os; 
+        ll ans = 0, c = 0;
+        for(int i = 0; i < n; i++){
+            c++;
+            os.insert(v[i]); 
+            ans += (c - os.order_of_key(v[i]) - 1);
+        }
+
+        cout << ans << endl;
+    }
+    return 0; 
+}
